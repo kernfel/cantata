5639a3f24cb834aad275bd5b8c10e2990d6da9f7
Felix B. Kern <kernfel+github@gmail.com>
2020-06-11 12:05:02 +0900

    Improved code structure (grouping params and definitions)

--- S2.py
+++ S2.py
@@ -83,7 +83,7 @@ STDP_defaults = {
     'alpha': 0.2        # Depression factor
 }
 
-#%% Parameters
+#%% Neurons
 
 params = dict()
 params['octaves'] = 1.
@@ -95,55 +95,6 @@ params_T['width'] = 0.2 # octaves; affects rate
 
 NT = 20*params['octaves']
 
-# ================= E =========================================
-params_E = {**params, **LIF_defaults}
-params_E['gL'] = 10 * nS
-params_E['tau'] = 20 * ms
-
-NE = 100*params['octaves']
-
-# ================= I =========================================
-params_I = {**params, **LIF_defaults}
-params_I['gL'] = 6 * nS
-params_I['tau'] = 20 * ms
-
-NI = 0.2*NE
-
-# ================= Network: Cortex ======================================
-params_synapses = params.copy()
-params_synapses['delay_per_oct'] = 5 * ms # per octave
-
-params_EE = {**params_synapses, **STDP_defaults}
-params_EE['gbar'] = 3 * nS
-params_EE['width_bin'] = 0.5 # octaves; binary connection probability
-params_EE['width'] = 0.1 # octaves; affects weight
-
-params_II = params_synapses.copy()
-params_II['gbar'] = 5 * nS
-params_II['width_bin'] = 0.5 # octaves; binary connection probability
-params_II['width'] = 0.1 # octaves; affects weight
-
-params_EI = params_synapses.copy()
-params_EI['gbar'] = 5 * nS
-params_EI['width_bin'] = 0.5 # octaves; binary connection probability
-params_EI['width'] = 0.2 # octaves; affects weight
-
-params_IE = params_synapses.copy()
-params_IE['gbar'] = 5 * nS
-params_IE['width_bin'] = 0.5 # octaves; binary connection probability
-params_IE['width'] = 0.2 # octaves; affects weight
-
-# ================== Network: Thalamo-cortical ===========================
-params_TE = params_synapses.copy()
-params_TE['weight'] = 1.1 * nS
-params_TE['width_p'] = 0.1 # octaves; affects connection probability
-
-params_TI = params_synapses.copy()
-params_TI['weight'] = 0.5 * nS
-params_TI['width_p'] = 0.15 # octaves; affects connection probability
-
-#%% Experimental setup
-
 exp_period = 2 * second
 T_rate = Equations('''
 rates = max_rate * exp(-alpha * (current_freq - best_freq)**2) : Hz
@@ -152,17 +103,23 @@ best_freq = i * 1.0/N : 1
 alpha = 1/(2*width**2) : 1
 ''')
 
-#%% Building the populations
-
-def build_populations():
+def build_T():
     T = NeuronGroup(NT, T_rate + 'x : 1',
                     threshold='rand()<rates*dt',
                     namespace = params_T,
                     name = 'Thalamus')
     T.x = 'i * octaves / N'
-    
-    eqn = LIF_eqn + 'x : 1'
-    E = NeuronGroup(NE, eqn, method='euler',
+    return T
+
+# ================= E =========================================
+params_E = {**params, **LIF_defaults}
+params_E['gL'] = 10 * nS
+params_E['tau'] = 20 * ms
+
+NE = 100*params['octaves']
+
+def build_E():
+    E = NeuronGroup(NE, LIF_eqn + 'x : 1', method='euler',
                     threshold = 'V > threshold',
                     reset = 'V = Vrest',
                     refractory = params_E['refractory'],
@@ -170,8 +127,17 @@ def build_populations():
                     name = 'Excitatory')
     E.V = params_E['Vrest']
     E.x = 'i * octaves / N'
-    
-    I = NeuronGroup(NI, eqn, method='euler',
+    return E
+
+# ================= I =========================================
+params_I = {**params, **LIF_defaults}
+params_I['gL'] = 6 * nS
+params_I['tau'] = 20 * ms
+
+NI = 0.2*NE
+
+def build_I():
+    I = NeuronGroup(NI, LIF_eqn + 'x : 1', method='euler',
                     threshold = 'V > threshold',
                     reset = 'V = Vrest',
                     refractory = params_I['refractory'],
@@ -179,8 +145,114 @@ def build_populations():
                     name = 'Inhibitory')
     I.V = params_I['Vrest']
     I.x = 'i * octaves / N'
-    
-    return T, E, I
+    return I
+
+#%% Network: Cortex
+params_synapses = params.copy()
+params_synapses['delay_per_oct'] = 5 * ms # per octave
+delay_eqn = 'delay_per_oct * abs(x_pre-x_post)'
+
+# ================= EE =========================================
+params_EE = {**params_synapses, **STDP_defaults}
+params_EE['gbar'] = 3 * nS
+params_EE['width_bin'] = 0.5 # octaves; binary connection probability
+params_EE['width'] = 0.1 # octaves; affects weight
+
+def build_EE(source, target):
+    EE = Synapses(source, target,
+                  model = Equations('weight : siemens') + STDP_eqn,
+                  on_pre = 'g_ampa_post += weight*w_stdp' + STDP_onpre,
+                  on_post = STDP_onpost,
+                  namespace = params_EE,
+                  name = 'Exc_Exc')
+    EE.connect(condition = 'i!=j and abs(x_pre-x_post) < width_bin')
+    EE.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
+    EE.w_stdp = 1
+    EE.delay = delay_eqn
+    return EE
+
+# ================= II =========================================
+params_II = params_synapses.copy()
+params_II['gbar'] = 5 * nS
+params_II['width_bin'] = 0.5 # octaves; binary connection probability
+params_II['width'] = 0.1 # octaves; affects weight
+
+def build_II(source, target):
+    II = Synapses(source, target,
+                  model = 'weight : siemens',
+                  on_pre = 'g_gaba_post += weight',
+                  namespace = params_II,
+                  name = 'Inh_Inh')
+    II.connect(condition = 'i!=j and abs(x_pre-x_post) < width_bin')
+    II.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
+    return II
+
+# ================= EI =========================================
+params_EI = params_synapses.copy()
+params_EI['gbar'] = 5 * nS
+params_EI['width_bin'] = 0.5 # octaves; binary connection probability
+params_EI['width'] = 0.2 # octaves; affects weight
+
+def build_EI(source, target):
+    EI = Synapses(source, target,
+                  model = 'weight : siemens',
+                  on_pre = 'g_ampa_post += weight',
+                  namespace = params_EI,
+                  name = 'Exc_Inh')
+    EI.connect(condition = 'abs(x_pre-x_post) < width_bin')
+    EI.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
+    return EI
+
+# ================= IE =========================================
+params_IE = params_synapses.copy()
+params_IE['gbar'] = 5 * nS
+params_IE['width_bin'] = 0.5 # octaves; binary connection probability
+params_IE['width'] = 0.2 # octaves; affects weight
+
+def build_IE(source, target):
+    IE = Synapses(source, target,
+                  model = 'weight : siemens',
+                  on_pre = 'g_gaba_post += weight',
+                  namespace = params_IE,
+                  name = 'Inh_Exc')
+    IE.connect(condition = 'abs(x_pre-x_post) < width_bin')
+    IE.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
+    return IE
+
+#%% Network: Thalamo-cortical
+
+# ================= TE =========================================
+params_TE = params_synapses.copy()
+params_TE['weight'] = 1.1 * nS
+params_TE['width_p'] = 0.1 # octaves; affects connection probability
+
+def build_TE(source, target):
+    TE = Synapses(source, target,
+                  on_pre = 'g_ampa_post += weight',
+                  namespace = params_TE,
+                  name='Thal_Exc')
+    TE.connect(p = 'exp(-(i*octaves/N_pre - j*octaves/N_post)**2 / (2*width_p**2))')
+    return TE
+
+# ================= TI =========================================
+params_TI = params_synapses.copy()
+params_TI['weight'] = 0.5 * nS
+params_TI['width_p'] = 0.15 # octaves; affects connection probability
+
+def build_TI(source, target):
+    TI = Synapses(source, target,
+                  on_pre = 'g_ampa_post += weight',
+                  namespace = params_TI,
+                  name='Thal_Inh')
+    TI.connect(p = 'exp(-(i*octaves/N_pre - j*octaves/N_post)**2 / (2*width_p**2))')
+    return TI
+
+#%% Building the populations
+
+def build_populations():
+    return build_T(), \
+           build_E(), \
+           build_I()
 
 def add_poisson(T, E, I):
     poisson_E = PoissonInput(E, 'g_ampa', params_E['poisson_N'],
@@ -194,55 +266,6 @@ def add_poisson(T, E, I):
 #%% Building the network
 
 def build_network(T, E, I):
-    delay_eqn = 'delay_per_oct * abs(x_pre-x_post)'
-    
-    TE = Synapses(T, E,
-                  on_pre = 'g_ampa_post += weight',
-                  namespace = params_TE,
-                  name='Thal_Exc')
-    TE.connect(p = 'exp(-(i*octaves/N_pre - j*octaves/N_post)**2 / (2*width_p**2))')
-    
-    TI = Synapses(T, I,
-                  on_pre = 'g_ampa_post += weight',
-                  namespace = params_TI,
-                  name='Thal_Inh')
-    TI.connect(p = 'exp(-(i*octaves/N_pre - j*octaves/N_post)**2 / (2*width_p**2))')
-    
-    EE = Synapses(E, E,
-                  model = Equations('weight : siemens') + STDP_eqn,
-                  on_pre = 'g_ampa_post += weight*w_stdp' + STDP_onpre,
-                  on_post = STDP_onpost,
-                  namespace = params_EE,
-                  name = 'Exc_Exc')
-    EE.connect(condition = 'i!=j and abs(x_pre-x_post) < width_bin')
-    EE.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
-    EE.w_stdp = 1
-    EE.delay = delay_eqn
-    
-    EI = Synapses(E, I,
-                  model = 'weight : siemens',
-                  on_pre = 'g_ampa_post += weight',
-                  namespace = params_EI,
-                  name = 'Exc_Inh')
-    EI.connect(condition = 'abs(x_pre-x_post) < width_bin')
-    EI.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
-    
-    IE = Synapses(I, E,
-                  model = 'weight : siemens',
-                  on_pre = 'g_gaba_post += weight',
-                  namespace = params_IE,
-                  name = 'Inh_Exc')
-    IE.connect(condition = 'abs(x_pre-x_post) < width_bin')
-    IE.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
-    
-    II = Synapses(I, I,
-                  model = 'weight : siemens',
-                  on_pre = 'g_gaba_post += weight',
-                  namespace = params_II,
-                  name = 'Inh_Inh')
-    II.connect(condition = 'i!=j and abs(x_pre-x_post) < width_bin')
-    II.weight = 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'
-    
-    return TE, TI, \
-           EE, EI, \
-           IE, II
+    return build_TE(T, E), build_TI(T, I), \
+           build_EE(E, E), build_EI(E, I), \
+           build_IE(I, E), build_II(I, I)
