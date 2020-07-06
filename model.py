8ba6d6c32df6c73aefa99aa47973d249dd7319ce
Felix B. Kern <kernfel+github@gmail.com>
2020-07-06 15:43:18 +0900

    Upgrade model to two layers

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ Created on Fri May 22 2020
 #%% Startup
 from brian2 import *
 import defaults
+import copy
 
 params = {
     'scale': 100
@@ -38,20 +39,28 @@ alpha = 1/(2*width**2) : 1
     }
 }
 
-# ================= E =========================================
-pops['E'] = {**defaults.LIF, **defaults.localised_neuron, **params}
-pops['E']['gL'] = 10 * nS
-pops['E']['tau'] = 20 * ms
-pops['E']['_'] = {'build': {'name': 'Excitatory', 'N': params['scale']}}
+#%% Cortex, stage 1
 
-# ================= I =========================================
-pops['I'] = {**defaults.LIF, **defaults.localised_neuron, **params}
-pops['I']['gL'] = 6 * nS
-pops['I']['tau'] = 20 * ms
-pops['I']['_'] = {'build': {'name': 'Inhibitory', 'N': 0.2*params['scale']}}
+# ======================== Layer 2/3 =========================================
+pops['S1_L23_Ex'] = {**defaults.LIF, **defaults.localised_neuron, **params}
+pops['S1_L23_Ex']['gL'] = 10 * nS
+pops['S1_L23_Ex']['tau'] = 20 * ms
+pops['S1_L23_Ex']['_'] = {'build': {'name': 'S1_L23_Ex', 'N': params['scale']}}
+
+pops['S1_L23_Inh'] = {**defaults.LIF, **defaults.localised_neuron, **params}
+pops['S1_L23_Inh']['gL'] = 6 * nS
+pops['S1_L23_Inh']['tau'] = 20 * ms
+pops['S1_L23_Inh']['_'] = {'build': {'name': 'S1_L23_Inh', 'N': 0.2*params['scale']}}
+
+# ======================== Layer 5 ===========================================
+pops['S1_L5_Ex'] = copy.deepcopy(pops['S1_L23_Ex'])
+pops['S1_L5_Ex']['_']['build']['name'] = 'S1_L5_Ex'
+
+pops['S1_L5_Inh'] = copy.deepcopy(pops['S1_L23_Inh'])
+pops['S1_L5_Inh']['_']['build']['name'] = 'S1_L5_Inh'
 
 
-#%% Network: Cortex
+#%% Network: Cortex, stage 1
 
 params_synapses = params.copy()
 params_synapses['delay_per_oct'] = 5 * ms # per full patch width
@@ -61,12 +70,12 @@ params_synapses['delay_f'] = 2 # distance scaling factor for higher delay steps
 syns = {}
 
 # ================= EE =========================================
-syns['E:E'] = {**defaults.weighted_synapse, **defaults.STDP, **defaults.varela_DD, **params_synapses}
-syns['E:E']['gbar'] = 3 * nS
-syns['E:E']['width'] = 0.1 # affects weight
+syns['S1_L23_Ex:S1_L23_Ex'] = {**defaults.weighted_synapse, **defaults.STDP, **defaults.varela_DD, **params_synapses}
+syns['S1_L23_Ex:S1_L23_Ex']['gbar'] = 3 * nS
+syns['S1_L23_Ex:S1_L23_Ex']['width'] = 0.1 # affects weight
+syns['S1_L23_Ex:S1_L23_Ex']['transmitter'] = 'ampa'
 
-syns['E:E']['_'] = {
-    'build': {'on_pre': 'g_ampa_post += {weight}'},
+syns['S1_L23_Ex:S1_L23_Ex']['_'] = {
     'connect': {
         'autapses': False,
         'maxdist': 0.5,
@@ -76,13 +85,16 @@ syns['E:E']['_'] = {
     'init': {'weight': 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'}
 }
 
-# ================= II =========================================
-syns['I:I'] = {**defaults.weighted_synapse, **params_synapses}
-syns['I:I']['gbar'] = 5 * nS
-syns['I:I']['width'] = 0.1 # affects weight
+syns['S1_L23_Ex:S1_L5_Ex'] = copy.deepcopy(syns['S1_L23_Ex:S1_L23_Ex'])
+syns['S1_L5_Ex:S1_L23_Ex'] = copy.deepcopy(syns['S1_L23_Ex:S1_L23_Ex'])
 
-syns['I:I']['_'] = {
-    'build': {'on_pre': 'g_gaba_post += {weight}'},
+# ================= II =========================================
+syns['S1_L23_Inh:S1_L23_Inh'] = {**defaults.weighted_synapse, **params_synapses}
+syns['S1_L23_Inh:S1_L23_Inh']['gbar'] = 5 * nS
+syns['S1_L23_Inh:S1_L23_Inh']['width'] = 0.1 # affects weight
+syns['S1_L23_Inh:S1_L23_Inh']['transmitter'] = 'gaba'
+
+syns['S1_L23_Inh:S1_L23_Inh']['_'] = {
     'connect': {
         'autapses': False,
         'maxdist': 0.3,
@@ -92,13 +104,15 @@ syns['I:I']['_'] = {
     'init': {'weight': 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'}
 }
 
-# ================= EI =========================================
-syns['E:I'] = {**defaults.weighted_synapse, **defaults.varela_DD, **params_synapses}
-syns['E:I']['gbar'] = 5 * nS
-syns['E:I']['width'] = 0.2 # affects weight
+syns['S1_L5_Inh:S1_L5_Inh'] = copy.deepcopy(syns['S1_L23_Inh:S1_L23_Inh'])
 
-syns['E:I']['_'] = {
-    'build': {'on_pre': 'g_ampa_post += {weight}'},
+# ================= EI =========================================
+syns['S1_L23_Ex:S1_L23_Inh'] = {**defaults.weighted_synapse, **defaults.varela_DD, **params_synapses}
+syns['S1_L23_Ex:S1_L23_Inh']['gbar'] = 5 * nS
+syns['S1_L23_Ex:S1_L23_Inh']['width'] = 0.2 # affects weight
+syns['S1_L23_Ex:S1_L23_Inh']['transmitter'] = 'ampa'
+
+syns['S1_L23_Ex:S1_L23_Inh']['_'] = {
     'connect': {
         'maxdist': 0.5,
         'p': 0.6,
@@ -107,14 +121,16 @@ syns['E:I']['_'] = {
     'init': {'weight': 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'}
 }
 
-# ================= IE =========================================
-syns['I:E'] = {**defaults.weighted_synapse, **defaults.STDP, **params_synapses}
-syns['I:E']['gbar'] = 5 * nS
-syns['I:E']['width'] = 0.2 # affects weight
-syns['I:E']['etapost'] =  syns['I:E']['etapre']
+syns['S1_L5_Ex:S1_L5_Inh'] = copy.deepcopy(syns['S1_L23_Ex:S1_L23_Inh'])
 
-syns['I:E']['_'] = {
-    'build': {'on_pre': 'g_gaba_post += {weight}'},
+# ================= IE =========================================
+syns['S1_L23_Inh:S1_L23_Ex'] = {**defaults.weighted_synapse, **defaults.STDP, **params_synapses}
+syns['S1_L23_Inh:S1_L23_Ex']['gbar'] = 5 * nS
+syns['S1_L23_Inh:S1_L23_Ex']['width'] = 0.2 # affects weight
+syns['S1_L23_Inh:S1_L23_Ex']['transmitter'] = 'gaba'
+syns['S1_L23_Inh:S1_L23_Ex']['etapost'] =  syns['S1_L23_Inh:S1_L23_Ex']['etapre']
+
+syns['S1_L23_Inh:S1_L23_Ex']['_'] = {
     'connect': {
         'maxdist': 0.3,
         'p': 0.9,
@@ -123,16 +139,19 @@ syns['I:E']['_'] = {
     'init': {'weight': 'gbar * exp(-(x_pre-x_post)**2/(2*width**2))'}
 }
 
+syns['S1_L5_Inh:S1_L5_Ex'] = copy.deepcopy(syns['S1_L23_Inh:S1_L23_Ex'])
+
 #%% Network: Thalamo-cortical
 
 # ================= TE =========================================
-syns['T:E'] = params_synapses.copy()
-syns['T:E']['weight'] = 1.1 * nS
-syns['T:E']['delay_per_oct'] = 0 * ms
-syns['T:E']['delay_k0'] = 1
+syns['T:S1_L23_Ex'] = params_synapses.copy()
+syns['T:S1_L23_Ex']['weight'] = 1.1 * nS
+syns['T:S1_L23_Ex']['delay_per_oct'] = 0 * ms
+syns['T:S1_L23_Ex']['delay_k0'] = 1
+syns['T:S1_L23_Ex']['transmitter'] = 'ampa'
 
-syns['T:E']['_'] = {
-    'build': {'on_pre': 'g_ampa_post += weight'},
+syns['T:S1_L23_Ex']['_'] = {
+    'build': {'on_pre': 'g_{transmitter}_post += weight'},
     'connect': {
         'distribution': 'normal',
         'sigma': 0.1,
@@ -140,13 +159,14 @@ syns['T:E']['_'] = {
 }
 
 # ================= TI =========================================
-syns['T:I'] = params_synapses.copy()
-syns['T:I']['weight'] = 0.5 * nS
-syns['T:I']['delay_per_oct'] = 0 * ms
-syns['T:I']['delay_k0'] = 1
+syns['T:S1_L23_Inh'] = params_synapses.copy()
+syns['T:S1_L23_Inh']['weight'] = 0.5 * nS
+syns['T:S1_L23_Inh']['delay_per_oct'] = 0 * ms
+syns['T:S1_L23_Inh']['delay_k0'] = 1
+syns['T:S1_L23_Inh']['transmitter'] = 'ampa'
 
-syns['T:I']['_'] = {
-    'build': {'on_pre': 'g_ampa_post += weight'},
+syns['T:S1_L23_Inh']['_'] = {
+    'build': {'on_pre': 'g_{transmitter}_post += weight'},
     'connect': {
         'distribution': 'normal',
         'sigma': 0.15,
