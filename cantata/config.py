e33a7d3aab6abf79a6c38e62b1a52ccf14e808a4
Felix B. Kern <kernfel+github@gmail.com>
2020-12-01 14:45:44 +0900

    Added config sanitisation

--- cantata/config.py
+++ cantata/config.py
@@ -1,5 +1,6 @@
 import yaml
 import re
+import box
 from box import Box
 from pathlib import Path
 import torch
@@ -35,8 +36,6 @@ def read_config(master):
         supplied directly in 'model' and 'train' entries, or by referring to
         subordinate files in the 'model_config' and 'train_config' entries, respectively.
         In that case, existing 'model' or 'train' entries will be overwritten.
-        If a 'tspec' entry is not present, it will be populated with sensible
-        defaults (device=cuda:0, dtype=float).
         * If @arg master is path_like, data is read from the indicated YAML file,
         and subordinate file paths are resolved from its parent directory.
         * Otherwise, subordinate file paths are resolved from the working directory.
@@ -52,9 +51,7 @@ def read_config(master):
         conf.model = read_file(dir / conf.model_config)
     if 'train_config' in conf:
         conf.train = read_file(dir / conf.train_config)
-    if 'tspec' not in conf:
-        conf.tspec = Box(dict(device=torch.device('cuda:0'), dtype=torch.float))
-    return conf
+    return sanitise(conf)
 
 def load(master):
     '''
@@ -73,5 +70,82 @@ def reload():
     '''
     load(_latest_master)
 
+def sanitise(conf):
+    '''
+    Validates a full configuration against cfg_defaults, filling in any missing
+    values and aligning existing entries' types with the corresponding default's.
+    '''
+    sanitise_section(conf, cfg_defaults.base)
+    sanitise_section(conf.model, cfg_defaults.model)
+    for pop in conf.model.populations.values():
+        sanitise_section(pop, cfg_defaults.population)
+        for target in pop.targets.values():
+            sanitise_section(target, cfg_defaults.target)
+
+    sanitise_section(conf.tspec, cfg_defaults.tspec, False)
+    if type(conf.tspec.device) != torch.device:
+        conf.tspec.device = torch.device(conf.tspec.device)
+    if type(conf.tspec.dtype) != torch.dtype:
+        conf.tspec.dtype = torch_types[conf.tspec.dtype]
+
+    return conf
+
+def sanitise_section(section, section_defaults, typecheck = True):
+    '''
+    Validates a config section, filling in any missing values and aligning
+    existing entries' types with the corresponding default's.
+    '''
+    for key, value in section_defaults.items():
+        if key not in section:
+            section[key] = value
+        elif typecheck and type(section[key]) != type(value):
+            if section[key] == None:
+                section[key] = value
+            else:
+                try:
+                    section[key] = type(value)(section[key])
+                except box.exceptions.BoxValueError:
+                    raise TypeError('Failed to cast {}:{} from {} to {}'.format(
+                    key, section[key], type(section[key]), type(value)
+                    )) from None
+
+        if typecheck and type(value) == Box and 'name' in value:
+            for nested_key, nested_value in section[key].items():
+                if type(nested_value) != type(value.name):
+                    if nested_value == None:
+                        section[key][nested_key] = value.name
+                    else:
+                        try:
+                            section[key][nested_key] = type(value.name)(nested_value)
+                        except box.exceptions.BoxValueError:
+                            raise TypeError('Failed to cast {}:{}:{} from {} to {}'.format(
+                            key, nested_key, nested_value,
+                            type(nested_value), type(value.name)
+                            )) from None
+
+torch_types = {
+    'float': torch.float,
+    'float32': torch.float32,
+    'double': torch.double,
+    'float64': torch.float64,
+    'cfloat': torch.cfloat,
+    'complex64': torch.complex64,
+    'cdouble': torch.cdouble,
+    'complex128': torch.complex128,
+    'half': torch.half,
+    'float16': torch.float16,
+    'bfloat16': torch.bfloat16,
+    'uint8': torch.uint8,
+    'int8': torch.int8,
+    'short': torch.short,
+    'int16': torch.int16,
+    'int': torch.int,
+    'int32': torch.int32,
+    'long': torch.long,
+    'int64': torch.int64,
+    'bool': torch.bool
+}
+
 cfg = Box()
+cfg_defaults = read_file(Path(__file__).parent / 'configs' / 'defaults.yaml')
 load(Path(__file__).parent / 'configs' / 'base.yaml')
--- test/test_config.py
+++ test/test_config.py
@@ -5,6 +5,38 @@ from box import Box
 import torch
 import os
 
+@pytest.fixture()
+def defaults():
+    conf = Box({
+        'time_step': 1e-3,
+        'n_steps': 100,
+        'batch_size': 32,
+        'n_inputs': 20,
+        'n_outputs': 1,
+        'tspec': {'device': torch.device('cuda:0'), 'dtype':torch.float},
+        'model': {
+            'weight_scale': 10.0,
+            'tau_mem': 20e-3,
+            'tau_mem_out': 5e-3,
+            'tau_p': 100e-3,
+            'populations': {
+                'name': {
+                    'n': 1,
+                    'sign': 1,
+                    'p': 0.0,
+                    'targets': {
+                        'name': {
+                            'density': 1.0,
+                            'delay': 0.0
+                        }
+                    }
+                }
+            }
+        },
+        'train': {}
+    })
+    return conf
+
 @pytest.fixture(scope='module')
 def dummy(tmp_path_factory):
     tmp_path = tmp_path_factory.mktemp('config.dummy')
@@ -14,17 +46,20 @@ def dummy(tmp_path_factory):
     })
     model = Box({'data': 0})
     train = Box({'data': 1})
-    expected = main + {
-        'model': model,
-        'train': train,
-        'tspec': dict(device=torch.device('cuda:0'), dtype=torch.float)
-    }
+
     main_path = tmp_path / 'main.yaml'
     model_path = tmp_path / main.model_config
     main.to_yaml(main_path)
     model.to_yaml(model_path)
     train.to_yaml(tmp_path / main.train_config)
-    return Box(dict(main=main, expected=expected,
+
+    unsanitised = main + {
+        'model': model,
+        'train': train
+    }
+    expected = unsanitised.copy()
+    config.sanitise(expected)
+    return Box(dict(main=main, unsanitised=unsanitised, expected=expected,
                     main_path=main_path, model_path=model_path))
 
 @pytest.fixture(scope='module')
@@ -60,11 +95,76 @@ def test_default_config_has_train():
     assert len(config.cfg.train) > 0, 'Must load sensible defaults'
 
 def test_read_file(dummy):
-    assert config.read_file(dummy.model_path) == dummy.expected.model
+    assert config.read_file(dummy.model_path) == dummy.unsanitised.model
 
 def test_read_file_scientific_notation(scientific):
     assert config.read_file(scientific.path) == scientific.floats, \
         'YML-compliant scientific notation is not read correctly'
+
+@pytest.fixture
+def template():
+    return Box(dict(
+        an_int = 1,
+        a_float = 1.2,
+        a_list = [],
+        a_string = 'asdf',
+        a_dict = {},
+        a_nested_dict = {'name':{}}
+    ))
+
+def test_sanitise_section_replicates_template(template):
+    conf = Box()
+    config.sanitise_section(conf, template)
+    assert conf == template
+
+def test_sanitise_section_retains_data(template):
+    filled = template.copy()
+    filled.arbitrary_data = True
+    filled.an_int = -8
+    filled.a_float = 1.4e-9
+    filled.a_list = [1,2,'no list item type checks']
+    filled.a_string = 'some other string'
+    filled.a_dict = dict(a=2, b=1)
+    filled.a_nested_dict = {'foo': {'tired': 1}, 'spam': {'wired': True}}
+    copy = filled.copy()
+    config.sanitise_section(filled, template)
+    assert filled == copy
+
+def test_sanitise_section_without_typechecks(template):
+    perversion = template.copy()
+    perversion.an_int = 'not an int'
+    perversion.a_float = 3
+    perversion.a_list = 4.5
+    perversion.a_string = {}
+    perversion.a_dict = []
+    perversion.a_nested_dict = 'absolutely not'
+    copy = perversion.copy()
+    config.sanitise_section(perversion, template, False)
+    assert perversion == copy
+
+def test_sanitise_empty_nested_dicts_remain_empty(defaults):
+    conf = defaults.copy()
+    conf.model.populations.clear()
+    copy = conf.copy()
+    config.sanitise(conf)
+    assert conf == copy
+
+def test_sanitise_fills_nested_entries_from_None(defaults):
+    conf = defaults.copy()
+    conf.model.populations.other_name = None
+    defaults.model.populations.other_name = defaults.model.populations.name
+    config.sanitise(conf)
+    assert conf == defaults
+
+def test_sanitise_empty_input_yields_defaults(defaults):
+    conf = Box()
+    config.sanitise(conf)
+    assert conf == defaults
+
+def test_sanitise_accepts_defaults_unchanged(defaults):
+    conf = defaults.copy()
+    config.sanitise(conf)
+    assert conf == defaults
 
 def test_read_config_path(dummy):
     assert config.read_config(dummy.main_path) == dummy.expected
@@ -90,10 +190,6 @@ def test_reload(dummy, reset):
     dummy.expected.model.to_yaml(dummy.model_path)
     config.reload()
     assert config.cfg == dummy.expected
-
-def test_read_config_preserves_master_defaults():
-    main = Box(dict(model='something', train='something else', tspec='???'))
-    assert config.read_config(main) == main
 
 def test_read_config_overwrites_subordinates(dummy):
     main = dummy.main.copy()
