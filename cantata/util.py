c6d1cdb0533f6d380445907c6c0b3b0273e7744c
Felix B. Kern <kernfel+github@gmail.com>
2021-02-26 20:41:39 +0900

    Do not force sunflower() onto non-default device

--- cantata/util.py
+++ cantata/util.py
@@ -20,9 +20,8 @@ def sunflower(n, alpha=1):
     Returns a sunflower seed pattern of n points. alpha>1 causes truncation of
     radii at the outer edge, making a smoother boundary.
     '''
-    device = torch.device('cuda' if torch.has_cuda else 'cpu')
-    b = torch.tensor(np.round(alpha * np.sqrt(n)), device=device)
-    k = torch.arange(1,n+1, device=device)
+    b = torch.tensor(np.round(alpha * np.sqrt(n)))
+    k = torch.arange(1,n+1)
     r = torch.sqrt(k-1/2) / torch.sqrt(n - (b+1)/2)
     r[r>1] = 1
     theta = 2*np.pi*k / phi**2
