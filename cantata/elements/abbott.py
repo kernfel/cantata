503413bb4a4f89d93d415e002f5017beed0af5b7
Felix B. Kern <kernfel+github@gmail.com>
2021-02-26 13:01:43 +0900

    Gate STDP via settings

--- cantata/elements/abbott.py
+++ cantata/elements/abbott.py
@@ -14,17 +14,20 @@ class Abbott(torch.nn.Module):
         shape = (cfg.batch_size, N)
 
         # Parameters
-        self.register_buffer('A_p', init.expand_to_synapses('A_p', projections),
-            persistent = False)
-        self.register_buffer('A_d', init.expand_to_synapses('A_d', projections),
-            persistent = False)
         self.alpha_p = util.decayconst(cfg.model.tau_p)
         self.alpha_d = util.decayconst(cfg.model.tau_d)
         self.wmax = cfg.model.wmax
+        A_p = init.expand_to_synapses('A_p', projections)
+        A_d = init.expand_to_synapses('A_d', projections)
+        self.active = torch.any(A_p != 0) or torch.any(A_d != 0)
+        if self.active:
+            self.register_buffer('A_p', A_p, persistent = False)
+            self.register_buffer('A_d', A_d, persistent = False)
 
         # State
-        self.register_buffer('xbar_pre', torch.zeros(shape))
-        self.register_buffer('xbar_post', torch.zeros(shape))
+        if self.active:
+            self.register_buffer('xbar_pre', torch.zeros(shape))
+            self.register_buffer('xbar_post', torch.zeros(shape))
         self.register_buffer('W', torch.zeros(shape + (N,)))
 
     def forward(self, Xpre, Xpost):
@@ -34,9 +37,12 @@ class Abbott(torch.nn.Module):
         Output: Synaptic weight before the update (batch, pre, post)
         '''
         W = self.W.clone()
-        dW_pot = torch.einsum('bo,beo,eo->beo', Xpost, self.xbar_pre, self.A_p)
-        dW_dep = torch.einsum('beo,bo,eo->beo', Xpre, self.xbar_post, self.A_d)
-        self.xbar_pre = util.expfilt(Xpre, self.xbar_pre, self.alpha_p)
-        self.xbar_post = util.expfilt(Xpost, self.xbar_post, self.alpha_d)
-        self.W = torch.clamp(W + dW_pot - dW_dep, 0, self.wmax)
+        if self.active:
+            dW_pot = torch.einsum('bo,   beo,           eo      ->beo',
+                                  Xpost, self.xbar_pre, self.A_p)
+            dW_dep = torch.einsum('beo, bo,             eo      ->beo',
+                                  Xpre, self.xbar_post, self.A_d)
+            self.xbar_pre = util.expfilt(Xpre, self.xbar_pre, self.alpha_p)
+            self.xbar_post = util.expfilt(Xpost, self.xbar_post, self.alpha_d)
+            self.W = torch.clamp(W + dW_pot - dW_dep, 0, self.wmax)
         return W
--- cantata/elements/syn_delta.py
+++ cantata/elements/syn_delta.py
@@ -11,30 +11,30 @@ class DeltaSynapse(torch.nn.Module):
     '''
     def __init__(self, projections, delaymap):
         super(DeltaSynapse, self).__init__()
-        N = init.get_N()
 
-        # Parameters
-        self.register_buffer('STDP_frac',
-            init.expand_to_synapses('STDP_frac', projections),
-            persistent = False)
-        self.register_buffer('delaymap', delaymap, persistent=False)
-
+        # Weights
         self.wmax = cfg.model.wmax
         w = init.build_connectivity(projections) * self.wmax
         self.W = torch.nn.Parameter(w)
         self.register_buffer('signs', torch.zeros_like(w), persistent = False)
         self.align_signs()
 
-        # Models
+        # Short-term plasticity
         shortterm = ce.STP()
         self.has_STP = shortterm.active
         if self.has_STP:
             self.shortterm = shortterm
 
-        if cfg.model.STDP_Clopath:
-            self.longterm = ce.Clopath(projections)
-        else:
-            self.longterm = ce.Abbott(projections)
+        # Long-term plasticity
+        STDP_frac = init.expand_to_synapses('STDP_frac', projections)
+        STDP_model = ce.Clopath if cfg.model.STDP_Clopath else ce.Abbott
+        longterm = STDP_model(projections)
+        self.has_STDP = torch.any(STDP_frac > 0) and longterm.active
+        if self.has_STDP:
+            self.register_buffer('STDP_frac', STDP_frac, persistent = False)
+            self.longterm = longterm
+
+        self.register_buffer('delaymap', delaymap, persistent=False)
 
     def forward(self, Xd, X):
         '''
@@ -43,13 +43,20 @@ class DeltaSynapse(torch.nn.Module):
         Output: Current (batch, post)
         '''
         Xpre = torch.einsum('deo,dbe->beo', self.delaymap, Xd)
-        Wlong = self.longterm(Xpre, X)
-        W = self.signs * (self.W * (1-self.STDP_frac) + Wlong * self.STDP_frac)
+
+        if self.has_STDP:
+            Wlong = self.longterm(Xpre, X)
+            W = self.signs * \
+                (self.W * (1-self.STDP_frac) + Wlong * self.STDP_frac)
+        else:
+            W = self.signs * self.W
+
         if self.has_STP:
             Wshort = torch.einsum('deo,dbe->beo', self.shortterm(Xd)+1)
             I = torch.einsum('beo,beo,beo->bo', W, Xpre, Wshort)
         else:
             I = torch.einsum('beo,beo->bo', W, Xpre)
+
         return I
 
     def load_state_dict(self, *args, **kwargs):
