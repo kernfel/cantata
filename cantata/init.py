cb0c1afd1d3ee0e1844a0e3297f5f56ba280eaae
Felix B. Kern <kernfel+github@gmail.com>
2021-03-01 19:26:49 +0900

    Separate area.population by period, not colon

--- cantata/init.py
+++ cantata/init.py
@@ -65,7 +65,7 @@ def build_projections_xarea(sconf, area_pre, area_post):
     conf_pre, conf_post = sconf.area_pre, sconf.area_post
     names_pre, idx_pre = build_population_indices(conf_pre)
     names_post, idx_post = build_population_indices(conf_post)
-    qualified_names = [area_post + ':' + name for name in names_post]
+    qualified_names = [area_post + '.' + name for name in names_post]
     projection_indices, projection_params = [], []
     for sname, source in zip(names_pre, idx_pre):
         for qual_tname, tparams in conf_pre.populations[sname].targets.items():
@@ -145,7 +145,7 @@ def get_delays_xarea(conf, dt):
     delays_set = set()
     for pop in conf.populations.values():
         for tname, p in pop.targets.items():
-            if ':' in tname:
+            if '.' in tname:
                 d = delay_xarea(p.delay, dt)
                 delays_set.add(d)
     return sorted(list(delays_set))
