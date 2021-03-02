eee310edfa51023627f7447e0376d3296509d7b7
Felix B. Kern <kernfel+github@gmail.com>
2021-03-02 12:42:44 +0900

    Updated testing infrastructure

--- test/conftest.py
+++ test/conftest.py
@@ -6,51 +6,39 @@ import torch
 import numpy as np
 from cantata import config
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def conf_components():
     '''
     Loads all fixture configs for modular use in test-scope fixtures,
     and ensures cfg resets after testing this module.
     '''
-    original_master = config._latest_master
-
     path = Path(__file__).parent / 'fixtures' / 'config'
     filenames = os.listdir(path)
     bits = {}
     for f in filenames:
         base, suffix = f.split('.')
         bits[base] = config.read_file(path / f)
-    yield Box(bits)
-
-    config.load(original_master)
-
-@pytest.fixture(params = ['cpu', 'cuda:0'])
-def tspec(request):
-    return Box(dict(device=torch.device(request.param), dtype=torch.float))
+    return Box(bits)
 
 @pytest.fixture
-def model_1(conf_components, tspec):
-    conf = conf_components.base_1.copy()
-    conf.model = conf_components.model_1.copy()
-    conf.tspec = tspec
-    config.load(conf)
+def rconf():
+    return Box(dict(batch_size = 4, dt = 1e-3))
 
 @pytest.fixture
-def model_1_noisy(conf_components, tspec):
-    conf = conf_components.base_1.copy()
-    conf.model = conf_components.model_1.copy()
-    conf.model.populations.Exc1.noise_N = np.random.randint(500) + 500
-    conf.model.populations.Exc1.noise_rate = np.random.rand()/conf.time_step
-    conf.model.populations.Exc1.noise_weight = np.random.rand()/500
-    conf.model.populations.Inh1.noise_N = np.random.randint(500) + 500
-    conf.model.populations.Inh1.noise_rate = np.random.rand()/conf.time_step
-    conf.model.populations.Inh1.noise_weight = np.random.rand()/500
-    conf.tspec = tspec
-    config.load(conf)
+def model_1(conf_components):
+    return config.read(conf_components.model_1)
 
 @pytest.fixture
-def model_2(conf_components, tspec):
-    conf = conf_components.base_1.copy()
-    conf.model = conf_components.model_2.copy()
-    conf.tspec = tspec
-    config.load(conf)
+def model_2(conf_components):
+    return config.read(conf_components.model_2)
+
+@pytest.fixture
+def model_1_noisy(conf_components, rconf):
+    conf = conf_components.model_1.copy()
+    conf.areas.A1.populations.Exc.noise_N = np.random.randint(500) + 500
+    conf.areas.A1.populations.Exc.noise_rate = np.random.rand()/rconf.dt
+    conf.areas.A1.populations.Exc.noise_weight = np.random.rand()/500
+    conf.areas.A1.populations.Inh.noise_N = np.random.randint(500) + 500
+    conf.areas.A1.populations.Inh.noise_rate = np.random.rand()/rconf.dt
+    conf.areas.A1.populations.Inh.noise_weight = np.random.rand()/500
+    return config.read(conf)
