ad7c76ecdf7e999e01dec4c7e6d61d5b75f6eecd
Felix B. Kern <kernfel+github@gmail.com>
2021-03-02 17:57:24 +0900

    Delay PoissonInput, if requested

--- cantata/elements/poisson_input.py
+++ cantata/elements/poisson_input.py
@@ -1,5 +1,6 @@
 import torch
 from cantata import init
+import cantata.elements as ce
 
 class PoissonInput(torch.nn.Module):
     '''
@@ -10,10 +11,19 @@ class PoissonInput(torch.nn.Module):
     '''
     def __init__(self, conf, dt, name = 'Input'):
         super(PoissonInput, self).__init__()
-        conf.N = sum([p.n for p in conf.populations.values()])
+        self.N = sum([p.n for p in conf.populations.values()])
         self.dt, self.name = dt, name
         self.p_names, self.p_idx = init.build_population_indices(conf)
-        cmap = torch.zeros(conf.n_channels, conf.N)
+
+        # Add 1 to account for immediate delivery of truly delayed connections.
+        delays = [d+1 for d in init.get_delays(conf, dt, True)]
+        if delays[0] == 2: # Remove minimal delay, deliver it immediately
+            delays = delays[1:]
+        self.buffered = len(delays)
+        if self.buffered:
+            self.rate_buffer = ce.DelayBuffer((batch_size,conf.n_channels), delays)
+
+        cmap = torch.zeros(conf.n_channels, self.N)
         for pname, pidx in zip(self.p_names, self.p_idx):
             cmap[conf.populations[pname].channel, pidx] = 1
         self.register_buffer('cmap', cmap, persistent = False)
@@ -21,8 +31,16 @@ class PoissonInput(torch.nn.Module):
     def forward(self, rates):
         '''
         rates, in Hz: (batch, channels)
-        Output, in spikes: (batch, neurons)
+        Output, in spikes: (1, batch, neurons)
         '''
         norm_rates = torch.clip(rates * self.dt, 0, 1)
-        neuron_rates = norm_rates @ self.cmap # bc,cn->bn
+        if self.buffered:
+            delayed_rates, = self.rate_buffer(norm_rates)
+            norm_rates = torch.cat((
+                norm_rates.unsqueeze(0),
+                delayed_rates
+            ), dim=0)
+        else:
+            norm_rates = norm_rates.unsqueeze(0)
+        neuron_rates = torch.matmul(norm_rates, self.cmap) # dbc,cn->dbn
         return torch.bernoulli(neuron_rates)
