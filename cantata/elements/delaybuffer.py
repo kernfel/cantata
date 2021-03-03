5dc44d6efff509e1ca3420e2e55f6f62bca8dd63
Felix B. Kern <kernfel+github@gmail.com>
2021-03-03 15:22:57 +0900

    Improved DelayBuffer to conform to tests

--- cantata/elements/delaybuffer.py
+++ cantata/elements/delaybuffer.py
@@ -3,26 +3,40 @@ import torch
 class DelayBuffer(torch.nn.Module):
     def __init__(self, shape, *delay_lists):
         super(DelayBuffer, self).__init__()
-        self.t = 0
-        self.max_delay = max([i for part in delay_lists for i in part])
         self.delay_lists = delay_lists
-        for d in range(self.max_delay):
-            self.register_buffer(
-                f'delay_{d}', torch.zeros(shape))
+        alldelays = [i for part in delay_lists for i in part]
+        for i in alldelays:
+            if i < 0:
+                raise ValueError(f'Invalid delay: {i}')
+        self.active = len(alldelays) > 0
+        if self.active:
+            self.t = 0
+            self.max_delay = max(alldelays)
+            for d in range(self.max_delay):
+                self.register_buffer(
+                    f'delay_{d}', torch.zeros(shape))
 
     def reset(self):
-        self.t = 0
-        for d in range(self.max_delay):
-            setattr(self, f'delay_{d}', torch.zeros_like(self.delay_0))
+        if self.active:
+            self.t = 0
+            for d in range(self.max_delay):
+                setattr(self, f'delay_{d}', torch.zeros_like(self.delay_0))
 
     def forward(self, input):
-        out = [self.get_buffer(d) for d in self.delay_lists]
-        setattr(self, f'delay_{self.t % self.max_delay}', input)
-        self.t = self.t + 1
+        out = [self.get_buffer(d, input) for d in self.delay_lists]
+        if self.active:
+            setattr(self, f'delay_{self.t % self.max_delay}', input)
+            self.t = self.t + 1
         return out
 
-    def get_buffer(self, delays):
+    def get_buffer(self, delays, input):
+        if not self.active or len(delays) == 0:
+            return None
         out = []
         for d in delays:
-            out.append(getattr(self, f'delay_{(self.t-d) % self.max_delay}'))
-        return torch.stack(out, dim=0) if len(out) > 0 else None
+            if d == 0:
+                out.append(input)
+            else:
+                out.append(
+                    getattr(self, f'delay_{(self.t-d) % self.max_delay}'))
+        return torch.stack(out, dim=0)
