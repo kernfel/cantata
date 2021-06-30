b41f886a4a0e1f50c9d98e546f9094fb2f46efce
Felix B. Kern <kernfel+github@gmail.com>
2021-06-30 19:19:07 +0900

    Updated STDP unit tests

--- test/test_abbott.py
+++ test/test_abbott.py
@@ -10,9 +10,11 @@ class Host(torch.nn.Module):
         self.register_buffer('delaymap', delaymap)
         self.register_buffer('wmin', wmin)
         self.register_buffer('wmax', wmax)
+        W = torch.rand_like(wmin) * (wmax-wmin) + wmin
+        self.register_buffer('W', W)
 
 @pytest.fixture(params = [True, False], ids = ['xarea', 'internal'])
-def constructor(model1, request, batch_size, dt):
+def construct(model1, request, batch_size, dt):
     conf_pre = model1.areas.A1
     nPre = 5
     if request.param:
@@ -24,69 +26,74 @@ def constructor(model1, request, batch_size, dt):
         name_post = None
         nPost = 5
     projections = init.build_projections(conf_pre, conf_post, name_post)
+    ctor = projections, conf_pre, batch_size, nPre, nPost, dt
+    m = ce.Abbott(*ctor)
     delaymap = init.get_delaymap(projections, dt, conf_pre, conf_post)
-    wmin = torch.zeros(nPre, nPost)
     wmax = torch.rand(nPre, nPost)
+    wmin = torch.rand(nPre, nPost) * wmax
     host = Host(delaymap, wmin, wmax)
-    return projections, host, conf_pre, batch_size, nPre, nPost, dt
+    m.reset(host)
+    return m, host, ctor
 
-def test_Abbott_can_change_device(constructor, spikes):
-    m = ce.Abbott(*constructor)
-    host, _, batch_size, nPre, nPost = constructor[1:6]
+def test_Abbott_can_change_device(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
     for device in [torch.device('cuda'), torch.device('cpu')]:
-        Xd = spikes(d, batch_size, nPre).to(device)
-        Xpost = spikes(batch_size, nPost).to(device)
+        Xd = spikes(d,b,e).to(device)
+        Xpost = spikes(b,o).to(device)
         m.to(device)
         host.to(device)
         W = m(Xd, Xpost)
         assert W.device == Xd.device
 
-def test_Abbott_does_not_move_host(constructor):
-    host = constructor[1]
-    m = ce.Abbott(*constructor)
+def test_Abbott_does_not_move_host(construct):
+    m, host, constructor = construct
     dev = host.wmax.device
     for device in [torch.device('cuda'), torch.device('cpu')]:
         m.to(device)
         assert host.wmax.device == dev
 
-def test_Abbott_state(constructor, module_tests):
-    host, _, b,e,o = constructor[1:6]
+def test_Abbott_state(construct, module_tests):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
     module_tests.check_state(
-        ce.Abbott(*constructor),
+        m,
         ['xbar_pre', 'xbar_post', 'W'],
         [(d,b,e),    (b,o),   (b,e,o)]
     )
 
-def test_Abbott_reset_clears_xbar(constructor, module_tests):
+def test_Abbott_reset_clears_xbar(construct, module_tests):
+    m, host, constructor = construct
     module_tests.check_reset_clears(
-        ce.Abbott(*constructor),
+        m,
         'xbar_pre', 'xbar_post',
-        inputs = (constructor[1].wmax,)
+        inputs = (host,)
     )
 
-def test_Abbott_reset_resets_W_to_arg(constructor):
-    m = ce.Abbott(*constructor)
+def test_Abbott_reset_resets_W_to_arg(construct):
+    m, host, constructor = construct
     m.W = torch.rand_like(m.W)
-    expected = torch.rand_like(constructor[1].wmax)
-    m.reset(expected.clone())
-    batch = np.random.randint(constructor[3])
+    expected = host.W
+    m.reset(host)
+    batch = np.random.randint(len(m.W))
     assert torch.equal(m.W[batch], expected)
 
-def test_Abbott_deactivates_when_not_required(constructor):
+def test_Abbott_deactivates_when_not_required(construct):
     # Note that STDP_frac is not within the STDP model's responsibility.
+    *_, constructor = construct
+    b,e,o = constructor[2:5]
     (_, params) = constructor[0]
-    nPost = constructor[5]
     for p in params:
         p.A_p = p.A_d = 0.
     m = ce.Abbott(*constructor)
     assert not m.active
 
-def test_Abbott_returns_premodification_W(constructor, spikes):
-    host, _, b,e,o = constructor[1:6]
+def test_Abbott_returns_premodification_W(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Abbott(*constructor)
     expected = torch.rand_like(m.W)
     m.W = expected.clone()
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -95,10 +102,10 @@ def test_Abbott_returns_premodification_W(constructor, spikes):
     assert torch.equal(W, expected)
     assert not torch.equal(m.W, expected)
 
-def test_Abbott_bounds_W_on_forward(constructor, spikes):
-    host, _, b,e,o = constructor[1:6]
+def test_Abbott_ubounds_W_on_forward(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Abbott(*constructor)
     unbounded = torch.randn_like(m.W)
     m.W = unbounded.clone()
     m(spikes(d,b,e), spikes(b,o))
@@ -107,10 +114,22 @@ def test_Abbott_bounds_W_on_forward(constructor, spikes):
     assert torch.all(m.W[unbounded < 0] == 0)
     assert torch.all(m.W[batch][surpass] == host.wmax[surpass])
 
-def test_Abbott_potentiates_on_post(constructor):
-    host, _, b,e,o = constructor[1:6]
+def test_Abbott_does_not_lbound_W_on_forward(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Abbott(*constructor)
+    unbounded = torch.randn_like(m.W)
+    m.W = unbounded.clone()
+    m(spikes(d,b,e), spikes(b,o))
+    batch = np.random.randint(b)
+    underrun = unbounded[batch] < host.wmin
+    assert torch.all(m.W[unbounded < 0] == 0)
+    assert torch.all(m.W[batch][underrun] <= host.wmax[underrun])
+
+def test_Abbott_potentiates_on_post(construct):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
+    d = host.delaymap.shape[0]
     host.wmax = torch.ones_like(host.wmax)
     m.W = torch.zeros_like(m.W)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -132,10 +151,10 @@ def test_Abbott_potentiates_on_post(constructor):
     m(Xpre, Xpost)
     assert torch.equal(m.W, expected)
 
-def test_Abbott_depresses_on_pre(constructor):
-    host, _, b,e,o = constructor[1:6]
+def test_Abbott_depresses_on_pre(construct):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Abbott(*constructor)
     host.wmax = torch.ones_like(host.wmax)
     m.W = torch.ones_like(m.W)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -157,11 +176,13 @@ def test_Abbott_depresses_on_pre(constructor):
     m(Xpre, Xpost)
     assert torch.equal(m.W, expected)
 
-def test_Abbott_filters_Xpre_with_tau_p(constructor, spikes):
-    _, host, conf, b,e,o, dt = constructor
+def test_Abbott_filters_Xpre_with_tau_p(construct, spikes):
+    m, host, constructor = construct
+    proj, conf, b,e,o, dt = constructor
     conf.tau_p = np.random.rand()
-    d = host.delaymap.shape[0]
     m = ce.Abbott(*constructor)
+    m.reset(host)
+    d = host.delaymap.shape[0]
     Xpre, Xpost = spikes(d,b,e), spikes(b,o)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
     alpha = util.decayconst(conf.tau_p, dt)
@@ -169,11 +190,13 @@ def test_Abbott_filters_Xpre_with_tau_p(constructor, spikes):
     m(Xpre, Xpost)
     assert torch.allclose(m.xbar_pre, expected)
 
-def test_Abbott_filters_Xpost_with_tau_d(constructor, spikes):
-    _, host, conf, b,e,o, dt = constructor
+def test_Abbott_filters_Xpost_with_tau_d(construct, spikes):
+    m, host, constructor = construct
+    proj, conf, b,e,o, dt = constructor
     conf.tau_d = np.random.rand()
-    d = host.delaymap.shape[0]
     m = ce.Abbott(*constructor)
+    m.reset(host)
+    d = host.delaymap.shape[0]
     Xpre, Xpost = spikes(d,b,e), spikes(b,o)
     m.xbar_post = torch.rand_like(m.xbar_post)
     alpha = util.decayconst(conf.tau_d, dt)
--- test/test_clopath.py
+++ test/test_clopath.py
@@ -10,9 +10,11 @@ class Host(torch.nn.Module):
         self.register_buffer('delaymap', delaymap)
         self.register_buffer('wmin', wmin)
         self.register_buffer('wmax', wmax)
+        W = torch.rand_like(wmin) * (wmax-wmin) + wmin
+        self.register_buffer('W', W)
 
 @pytest.fixture(params = [True, False], ids = ['xarea', 'internal'])
-def constructor(model1, request, batch_size, dt):
+def construct(model1, request, batch_size, dt):
     conf_pre = model1.areas.A1
     nPre = 5
     if request.param:
@@ -24,70 +26,75 @@ def constructor(model1, request, batch_size, dt):
         name_post = None
         nPost = 5
     projections = init.build_projections(conf_pre, conf_post, name_post)
+    ctor = projections, conf_pre, batch_size, nPre, nPost, dt
+    m = ce.Clopath(*ctor)
     delaymap = init.get_delaymap(projections, dt, conf_pre, conf_post)
-    wmin = torch.zeros(nPre, nPost)
     wmax = torch.rand(nPre, nPost)
+    wmin = torch.rand(nPre, nPost) * wmax
     host = Host(delaymap, wmin, wmax)
-    return projections, host, conf_pre, batch_size, nPre, nPost, dt
+    m.reset(host)
+    return m, host, ctor
 
-def test_Clopath_can_change_device(constructor, spikes):
-    m = ce.Clopath(*constructor)
-    host, _, batch_size, nPre, nPost = constructor[1:6]
+def test_Clopath_can_change_device(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
     for device in [torch.device('cuda'), torch.device('cpu')]:
-        Xd = spikes(d, batch_size, nPre).to(device)
-        Xpost = spikes(batch_size, nPost).to(device)
-        Vpost = torch.rand(batch_size, nPost).to(device)
+        Xd = spikes(d,b,e).to(device)
+        Xpost = spikes(b,o).to(device)
+        Vpost = torch.rand(b,o).to(device)
         m.to(device)
         host.to(device)
         W = m(Xd, Xpost, Vpost)
         assert W.device == Xd.device
 
-def test_Clopath_does_not_move_host(constructor):
-    host = constructor[1]
-    m = ce.Clopath(*constructor)
+def test_Clopath_does_not_move_host(construct):
+    m, host, constructor = construct
     dev = host.wmax.device
     for device in [torch.device('cuda'), torch.device('cpu')]:
         m.to(device)
         assert host.wmax.device == dev
 
-def test_Clopath_state(constructor, module_tests):
-    host, _, b,e,o = constructor[1:6]
+def test_Clopath_state(construct, module_tests):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
     module_tests.check_state(
-        ce.Clopath(*constructor),
+        m,
         ['xbar_pre', 'u_dep', 'u_pot', 'W'],
         [(d,b,e),    (b,o), (b,o), (b,e,o)]
     )
 
-def test_Clopath_reset_clears_filters(constructor, module_tests):
+def test_Clopath_reset_clears_filters(construct, module_tests):
+    m, host, constructor = construct
     module_tests.check_reset_clears(
-        ce.Clopath(*constructor),
+        m,
         'xbar_pre', 'u_dep', 'u_pot',
-        inputs = (constructor[1].wmax,)
+        inputs = (host,)
     )
 
-def test_Clopath_reset_resets_W_to_arg(constructor):
-    m = ce.Clopath(*constructor)
+def test_Clopath_reset_resets_W_to_arg(construct):
+    m, host, constructor = construct
     m.W = torch.rand_like(m.W)
-    expected = torch.rand_like(constructor[1].wmax)
-    m.reset(expected.clone())
-    batch = np.random.randint(constructor[3])
+    expected = host.W
+    m.reset(host)
+    batch = np.random.randint(len(m.W))
     assert torch.equal(m.W[batch], expected)
 
-def test_Clopath_deactivates_when_not_required(constructor):
+def test_Clopath_deactivates_when_not_required(construct):
     # Note that STDP_frac is not within the STDP model's responsibility.
+    *_, constructor = construct
+    b,e,o = constructor[2:5]
     (_, params) = constructor[0]
-    nPost = constructor[5]
     for p in params:
         p.A_p = p.A_d = 0.
     m = ce.Clopath(*constructor)
     assert not m.active
 
-def test_Clopath_returns_premodification_W(constructor, spikes):
-    host, _, b,e,o = constructor[1:6]
+def test_Clopath_returns_premodification_W(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Clopath(*constructor)
     expected = torch.rand_like(m.W)
     m.W = expected.clone()
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -97,10 +104,10 @@ def test_Clopath_returns_premodification_W(constructor, spikes):
     assert torch.equal(W, expected)
     assert not torch.equal(m.W, expected)
 
-def test_Clopath_bounds_W_on_forward(constructor, spikes):
-    host, _, b,e,o = constructor[1:6]
+def test_Clopath_ubounds_W_on_forward(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Clopath(*constructor)
     unbounded = torch.randn_like(m.W)
     m.W = unbounded.clone()
     m(spikes(d,b,e), spikes(b,o), torch.rand(b,o))
@@ -109,10 +116,22 @@ def test_Clopath_bounds_W_on_forward(constructor, spikes):
     assert torch.all(m.W[unbounded < 0] == 0)
     assert torch.all(m.W[batch][surpass] == host.wmax[surpass])
 
-def test_Clopath_potentiates_on_post(constructor):
-    host, _, b,e,o = constructor[1:6]
+def test_Clopath_does_not_lbound_W_on_forward(construct, spikes):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Clopath(*constructor)
+    unbounded = torch.randn_like(m.W)
+    m.W = unbounded.clone()
+    m(spikes(d,b,e), spikes(b,o), torch.rand(b,o))
+    batch = np.random.randint(b)
+    underrun = unbounded[batch] < host.wmin
+    assert torch.all(m.W[unbounded < 0] == 0)
+    assert torch.all(m.W[batch][underrun] <= host.wmax[underrun])
+
+def test_Clopath_potentiates_on_post(construct):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
+    d = host.delaymap.shape[0]
     host.wmax = torch.ones_like(host.wmax)
     m.W = torch.zeros_like(m.W)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -137,10 +156,10 @@ def test_Clopath_potentiates_on_post(constructor):
     m(Xpre, Xpost, Vpost)
     assert torch.allclose(m.W, expected)
 
-def test_Clopath_depresses_on_pre(constructor):
-    host, _, b,e,o = constructor[1:6]
+def test_Clopath_depresses_on_pre(construct):
+    m, host, constructor = construct
+    b,e,o = constructor[2:5]
     d = host.delaymap.shape[0]
-    m = ce.Clopath(*constructor)
     host.wmax = torch.ones_like(host.wmax)
     m.W = torch.ones_like(m.W)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
@@ -163,11 +182,13 @@ def test_Clopath_depresses_on_pre(constructor):
     m(Xpre, Xpost, Vpost)
     assert torch.allclose(m.W, expected)
 
-def test_Clopath_filters_Xpre_with_tau_x(constructor, spikes):
-    _, host, conf, b,e,o, dt = constructor
+def test_Clopath_filters_Xpre_with_tau_x(construct, spikes):
+    m, host, constructor = construct
+    proj, conf, b,e,o, dt = constructor
     conf.tau_x = np.random.rand()
-    d = host.delaymap.shape[0]
     m = ce.Clopath(*constructor)
+    m.reset(host)
+    d = host.delaymap.shape[0]
     Xpre, Xpost, Vpost = spikes(d,b,e), spikes(b,o), torch.rand(b,o)
     m.xbar_pre = torch.rand_like(m.xbar_pre)
     alpha = util.decayconst(conf.tau_x, dt)
@@ -175,11 +196,13 @@ def test_Clopath_filters_Xpre_with_tau_x(constructor, spikes):
     m(Xpre, Xpost, Vpost)
     assert torch.allclose(m.xbar_pre, expected)
 
-def test_Clopath_filters_udep_with_tau_d(constructor, spikes):
-    _, host, conf, b,e,o, dt = constructor
+def test_Clopath_filters_udep_with_tau_d(construct, spikes):
+    m, host, constructor = construct
+    proj, conf, b,e,o, dt = constructor
     conf.tau_d = np.random.rand()
-    d = host.delaymap.shape[0]
     m = ce.Clopath(*constructor)
+    m.reset(host)
+    d = host.delaymap.shape[0]
     Xpre, Xpost, Vpost = spikes(d,b,e), spikes(b,o), torch.rand(b,o)
     m.u_dep = torch.rand_like(m.u_dep)
     alpha = util.decayconst(conf.tau_d, dt)
@@ -187,11 +210,13 @@ def test_Clopath_filters_udep_with_tau_d(constructor, spikes):
     m(Xpre, Xpost, Vpost)
     assert torch.allclose(m.u_dep, expected)
 
-def test_Clopath_filters_upot_with_tau_p(constructor, spikes):
-    _, host, conf, b,e,o, dt = constructor
+def test_Clopath_filters_upot_with_tau_p(construct, spikes):
+    m, host, constructor = construct
+    proj, conf, b,e,o, dt = constructor
     conf.tau_p = np.random.rand()
-    d = host.delaymap.shape[0]
     m = ce.Clopath(*constructor)
+    m.reset(host)
+    d = host.delaymap.shape[0]
     Xpre, Xpost, Vpost = spikes(d,b,e), spikes(b,o), torch.rand(b,o)
     m.u_pot = torch.rand_like(m.u_pot)
     alpha = util.decayconst(conf.tau_p, dt)
