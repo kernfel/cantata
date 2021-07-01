7733b56f5b23b16fd2e2ee86b730ac38ea972b02
Felix B. Kern <kernfel+github@gmail.com>
2021-07-01 16:48:25 +0900

    Fixed SynCurrent bugs

--- cantata/elements/syn_current.py
+++ cantata/elements/syn_current.py
@@ -1,4 +1,5 @@
 import torch
+from cantata import util, init
 
 class SynCurrent(torch.nn.Module):
     '''
@@ -8,11 +9,16 @@ class SynCurrent(torch.nn.Module):
     '''
     def __init__(self, conf, batch_size, dt):
         super(SynCurrent, self).__init__()
+        self.active = conf.tau_I > 0
+        if not self.active:
+            return
         N = init.get_N(conf)
         self.alpha = util.decayconst(conf.tau_I, dt)
         self.register_buffer('I', torch.zeros(batch_size, N))
 
     def reset(self):
+        if not self.active:
+            return
         self.I.zero_()
 
     def forward(self, impulses):
@@ -20,5 +26,7 @@ class SynCurrent(torch.nn.Module):
         impulses: (batch, post)
         Output: Current (batch, post)
         '''
+        if not self.active:
+            return impulses
         self.I = self.alpha*self.I + (1-self.alpha)*impulses
         return self.I
