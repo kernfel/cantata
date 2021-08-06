3db0e50616a45e8d23914389ae33e78fbc886d59
Felix B. Kern <kernfel+github@gmail.com>
2021-08-06 14:38:15 +0900

    Code style

--- cantata/elements/presynaptic_separation/membrane.py
+++ cantata/elements/presynaptic_separation/membrane.py
@@ -2,6 +2,7 @@ import torch
 from cantata import init
 import cantata.elements as ce
 
+
 class Membrane(ce.Membrane):
     def __init__(self, conf, batch_size, dt, **kwargs):
         N = init.get_N(conf)
@@ -15,13 +16,14 @@ class Membrane(ce.Membrane):
     def reset(self):
         super(Membrane, self).reset()
         # Divide the random reset evenly:
-        self.V_separate = self.V.unsqueeze(1).expand_as(self.V_separate) / self.nPre
+        self.V_separate = self.V.unsqueeze(
+            1).expand_as(self.V_separate) / self.nPre
 
     def forward(self, current, X=None):
         self.V_separate = self.V_separate * self.alpha + current
         super(Membrane, self).forward(current.sum(dim=1), X)
         with torch.no_grad():
-            self.V_separate[ # Can't use self.ref, it's half a step ahead!
+            self.V_separate[  # Can't use self.ref, it's half a step ahead!
                 (self.V == 0).unsqueeze(1).expand_as(self.V_separate)
             ] = 0
         return self.V
